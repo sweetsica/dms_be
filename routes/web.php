a7f5fc77c63b0c689c852cdc8e5942303aca960b
Snow Deep <barondeepsnow@gmail.com>
2023-08-04 16:11:36 +0700

    fix config

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\VersionController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RouteDirectionController;
+
 
 
 /*
