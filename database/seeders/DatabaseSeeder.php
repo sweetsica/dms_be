8afda106259c16302665f94e552f7c06f6b51b65
MinhNN <nguyenngocminh2003@gmail.com>
2023-08-07 22:43:26 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(PersonnelSeeder::class);
+        $this->call(CustomerSeeder::class);
+        $this->call(OfficeDepartmentTBSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
