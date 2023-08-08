b6d3d5d83100f39a427c0ae1b897e0120b5a7fdc
MinhNN <nguyenngocminh2003@gmail.com>
2023-08-08 08:40:47 +0700

    seeder position

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(PersonnelSeeder::class);
         $this->call(OfficeDepartmentTBSeeder::class);
+        $this->call(PositionDepartmentSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
