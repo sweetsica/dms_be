0989258d3b0a66cced6f496701bb2fd1bdaf374c
egamorft <huytung.htbn@gmail.com>
2023-08-11 13:56:05 +0700

    fix migrate seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RoleSeeder::class);
         $this->call(OfficeDepartmentTBHTSeeder::class);
-        // $this->call(PositionDepartmentTBSeeder::class);
+        $this->call(PositionDepartmentTBSeeder::class);
         
         $this->call(PersonelLevelSeeder::class);
         $this->call(PersonnelSeeder::class);
