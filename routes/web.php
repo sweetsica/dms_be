9e730136d0f2ab3581e756add2f669a3721943b5
hiep820 <f8tuthan00@gmail.com>
2023-08-19 08:39:04 +0700

    uplen

--- routes/web.php
+++ routes/web.php
@@ -117,7 +117,7 @@ Route::middleware(['auth.role'])->group(function () {
     Route::post('them-moi-chi-tiet/{id}', [ProductController::class, 'create'])->name('product.create');
     Route::post('them-san-pham-lien-quan/{id}', [ProductController::class, 'related'])->name('product.related');
     Route::post('xoa-chi-tiet/{id}', [ProductController::class, 'delete'])->name('product.deleted');
-    Route::get('export-pdf',[ProductController::class, 'export'])->name('product.export');
+    Route::get('export-pdf/{id}',[ProductController::class, 'export'])->name('product.export');
 
     Route::get('danh-sach-phien-ban', [VersionController::class, 'index'])->name('version.list');
     Route::post('them-moi-phien-ban', [VersionController::class, 'store'])->name('version.store');
