e79ef7e9baf598e6682d18317294c2165942d0f1
MinhNN <nguyenngocminh2003@gmail.com>
2023-08-30 08:29:37 +0700

    Fix loc KH

--- resources/views/Customer/danhSachKhachHang.blade.php
+++ resources/views/Customer/danhSachKhachHang.blade.php
@@ -394,7 +394,7 @@
                     <h5 class="modal-title w-100" id="exampleModalLabel">Lọc dữ liệu</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <form method="POST" action="{{ route('create-customer') }}">
+                <form method="POST" action="">
                     @csrf
                     <div class="modal-body">
                         <div class="row">
@@ -404,7 +404,7 @@
                                     <select id="select-status" class="selectpicker select_filter"
                                         data-dropup-auto="false" title="Lọc theo nhóm khách hàng" name='nhomKH'>
                                         @foreach ($listgroup as $item)
-                                            <option value="{{ $item->name }}">{{ $item->name }}</option>
+                                            <option value="{{ $item->id }}">{{ $item->name }}</option>
                                         @endforeach
                                     </select>
                                 </div>
@@ -415,7 +415,7 @@
                                     <select id="select-status" class="selectpicker select_filter"
                                         data-dropup-auto="false" title="Lọc theo kênh khách hàng" name='kenhKH'>
                                         @foreach ($listChannel as $item)
-                                            <option value="{{ $item->name }}">{{ $item->name }}</option>
+                                            <option value="{{ $item->id }}">{{ $item->name }}</option>
                                         @endforeach
                                     </select>
                                 </div>
@@ -426,7 +426,7 @@
                                     <select id="select-status" class="selectpicker select_filter"
                                         data-dropup-auto="false" title="Lọc theo tuyến khách hàng" name='tuyenKH'>
                                         @foreach ($listRoute as $item)
-                                            <option value="{{ $item->name }}">{{ $item->name }}</option>
+                                            <option value="{{ $item->id }}">{{ $item->name }}</option>
                                         @endforeach
                                     </select>
                                 </div>
@@ -437,8 +437,32 @@
                                     <select id="select-status" class="selectpicker select_filter"
                                         data-dropup-auto="false" title="Lọc theo nhân sự thu thập" name='nhansutt'>
                                         @foreach ($listPersons as $item)
-                                            <option value="{{ $item->name }}">{{ $item->name }}</option>
+                                            <option value="{{ $item->id }}">{{ $item->name }}</option>
                                         @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 mb-3">
+                                <div data-bs-toggle="tooltip" data-bs-placement="top"
+                                    data-bs-original-title="Lọc theo tỉnh">
+                                    <select id="city" class="selectpicker"
+                                        data-dropup-auto="false" title="Lọc theo tỉnh" name='cityFilter'>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 mb-3">
+                                <div data-bs-toggle="tooltip" data-bs-placement="top"
+                                    data-bs-original-title="Lọc theo quận">
+                                    <select id="district" class="selectpicker select_filter"
+                                        data-dropup-auto="false" title="Lọc theo nhân quận" name='districtFilter'>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 mb-3">
+                                <div data-bs-toggle="tooltip" data-bs-placement="top"
+                                    data-bs-original-title="Lọc theo xã">
+                                    <select id="guide" class="selectpicker select_filter"
+                                        data-dropup-auto="false" title="Lọc theo xã" name='guideFilter'>
                                     </select>
                                 </div>
                             </div>
